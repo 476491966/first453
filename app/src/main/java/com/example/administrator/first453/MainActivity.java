81c0be88afbeeb60660f81abe865943ed60c3367
476491966 <476491966@qq.com>
2016-09-30 15:12:04 +0800

    initData

---
+++ app/src/main/java/com/example/administrator/first453/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.administrator.first453;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initData();
+        
+    }
+
+    private void initData() {
+
+    }
+
+    private void initView() {
+
+    }
+}
